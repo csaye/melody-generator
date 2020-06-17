ebcce98f6e8a486e3150207ff87a2ca998c3361e
Cooper Saye <coopersaye@gmail.com>
2020-06-17 13:53:35 -0700

    Implement basic variation algorithm.

--- Assets/Scripts/AIPiano.cs
+++ Assets/Scripts/AIPiano.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class AIPiano : MonoBehaviour
 {
     [Header("Attributes")]
-    [Range(0.1f, 2)] public float speed;
+    [Range(0, 1)] public float delay;
+    [Range(0, 1)] public float variation;
 
     [Header("Notes")]
     public List<AudioClip> notes;
@@ -13,25 +14,72 @@ public class AIPiano : MonoBehaviour
     [Header("References")]
     public AudioSource audioSource;
 
-    // Start is called before the first frame update
+    private bool playing = false;
+    
+    private Coroutine cor;
+
+    private int maxVariation;
+
     void Start()
     {
-        StartCoroutine(playNote());
+        maxVariation = (int)(variation * notes.Count);
+        
+        // Max variation failsafe
+        if (maxVariation < 0)
+        {
+            maxVariation = 0;
+        }
+        if (maxVariation > notes.Count - 1)
+        {
+            maxVariation = notes.Count - 1;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("space"))
+        {
+            playing = !playing;
+
+            if (playing)
+            {
+                cor = StartCoroutine(playNote());
+            }
+            else
+            {
+                StopCoroutine(cor);
+            }
+        }
     }
 
-    IEnumerator playNote()
+    private IEnumerator playNote()
     {
-        for (int i = 0; i < notes.Count; i++)
+        // Begin on random note
+        int index = Random.Range(0, notes.Count - 1);
+
+        while (true)
         {
-            yield return new WaitForSeconds(speed);
-            audioSource.clip = notes[i];
+            audioSource.clip = notes[index];
             audioSource.Play();
+            index = modifyIndex(index);
+            yield return new WaitForSeconds(delay);
         }
+    }
+
+    private int modifyIndex(int index)
+    {
+        index += Random.Range(maxVariation * -1, maxVariation);
+
+        // Index failsafe
+        if (index < 0)
+        {
+            index = 0;
+        }
+        if (index > notes.Count - 1)
+        {
+            index = notes.Count - 1;
+        }
+
+        return index;
     }
 }
