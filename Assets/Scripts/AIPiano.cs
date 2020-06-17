80c037a87258ca879293858a1cdf392a08ec86a0
Cooper Saye <coopersaye@gmail.com>
2020-06-17 13:34:16 -0700

    Import piano notes.

--- Assets/Scripts/AIPiano.cs
+++ Assets/Scripts/AIPiano.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class AIPiano : MonoBehaviour
 {
-    public List<AudioClip>();
+    [Header("Attributes")]
+    [Range(0.1f, 2)] public float speed;
+
+    [Header("Notes")]
+    public List<AudioClip> notes;
+
+    [Header("References")]
+    public AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(playNote());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator playNote()
+    {
+        for (int i = 0; i < notes.Count; i++)
+        {
+            yield return new WaitForSeconds(speed);
+            audioSource.clip = notes[i];
+            audioSource.Play();
+        }
     }
 }
